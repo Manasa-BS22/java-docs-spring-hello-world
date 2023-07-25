965c82affe459a89b815d8c10404432b358a7a94
Manasa Somashekar <mansomas@in.ibm.com>
2023-07-25 22:09:01 +0530

    removing controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	String sayHello() {
+		return "Hello World!";
+	}
 }
